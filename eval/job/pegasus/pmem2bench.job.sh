a65bb3c5aac0a73885bf59b3872e0a010bbf7806
range3 <white.line.oar@gmail.com>
2023-06-23 12:52:27 +0900

    Update job script

--- eval/job/pegasus/pmem2bench.job.sh
+++ eval/job/pegasus/pmem2bench.job.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#PBS -A NBB
+#PBS -q gpu
+#PBS -l elapstim_req=1:00:00
+#PBS -T distrib 
+
 set -eux
 
 function timestamp() {
@@ -11,9 +16,12 @@ function addtimestamp() {
     done
 }
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE:-$0}" )" &> /dev/null && pwd )"
-PROJECT_DIR="$( cd "${SCRIPT_DIR}/../../.." &> /dev/null && pwd )"
-OUTPUT_DIR="${PROJECT_DIR}/eval/raw/pegasus/pmem2bench/$(timestamp)"
+param_source=$PMEM2BENCH_SOURCE
+# nthreads=$PMEM2BENCH_NTHREADS
+block_size=$PMEM2BENCH_BLOCK_SIZE
+ntstore=$PMEM2BENCH_NTSTORE
+access_pattern=$PMEM2BENCH_ACCESS_PATTERN
+
 mkdir -p "${OUTPUT_DIR}"
 # SCRIPT_FILE_NAME="$( basename -- "$0" )"
 # JOB_FILE="${SCRIPT_DIR}/${SCRIPT_FILE_NAME%.*}.job.sh"
@@ -23,45 +31,65 @@ spack env activate -d spack/envs/pegasus
 
 BENCHMARK_EXE="${PROJECT_DIR}/install/bin/pmem2bench"
 
-block_list=(
-  256 512
-  1K 2K 4K 8K 16K 32K 64K 128K 256K 512K
-  1M 2M 4M 8M 16M
-)
+size_per_thread=$((8*2**30))
+
+ntstore_opt=""
+ntstore_label="t"
+if [ "${ntstore}" = true ]; then
+  ntstore_opt="--non-temporal"
+  ntstore_label="nt"
+fi
+random_access_opt=""
+if [ "${access_pattern}" = "random" ]; then
+  random_access_opt="--random"
+fi
+
+file_path="/dev/dax0.0"
+if [ "${param_source}" = "fsdax" ]; then
+  file_path="/pmem/pmem2bench.bin"
+fi
+
 
 nthreads_list=(
   1 2 4 8 16 32 48
-  # 16
 )
 
-size_per_thread=$((8*2**30))
+max_iter=3
 
 for nthreads in "${nthreads_list[@]}"; do
-for block in "${block_list[@]}"; do
-  total_size=$((nthreads * size_per_thread))
+total_size=$((nthreads * size_per_thread))
+for iter in $(seq $max_iter); do
   "${BENCHMARK_EXE}" \
-    --source devdax \
-    --path /dev/dax0.0 \
+    --source $param_source \
+    --path $file_path \
     --nthreads $nthreads \
-    --file_size $total_size \
     --total $total_size \
     --stripe $total_size \
-    --block $block \
-    --non-temporal \
+    --block $block_size \
+    $ntstore_opt \
+    $random_access_opt \
     --write \
     --prettify \
-      > "${OUTPUT_DIR}/w_${block}_${nthreads}.json"
-  "${BENCHMARK_EXE}" \
-    --source devdax \
-    --path /dev/dax0.0 \
-    --nthreads $nthreads \
-    --file_size $total_size \
-    --total $total_size \
-    --stripe $total_size \
-    --block $block \
-    --non-temporal \
-    --read \
-    --prettify \
-      > "${OUTPUT_DIR}/r_${block}_${nthreads}.json"
+      > "${OUTPUT_DIR}/w_${access_pattern}_${ntstore_label}_${param_source}_${block_size}_${nthreads}_${iter}.json"
+
+  if [ "${ntstore}" = true ]; then
+    "${BENCHMARK_EXE}" \
+      --source $param_source \
+      --path $file_path \
+      --nthreads $nthreads \
+      --total $total_size \
+      --stripe $total_size \
+      --block $block_size \
+      $ntstore_opt \
+      $random_access_opt \
+      --read \
+      --prettify \
+        > "${OUTPUT_DIR}/r_${access_pattern}_${param_source}_${block_size}_${nthreads}_${iter}.json"
+  fi
 done
+
+if [ "${param_source}" = "fsdax" ]; then
+  rm $file_path
+fi
+
 done
--- eval/job/pegasus/pmem2bench.sh
+++ eval/job/pegasus/pmem2bench.sh
@@ -1,13 +1,66 @@
 #!/bin/bash
 
-for nthreads in 1 2 4 8 16; do
-for block_size in 64 128 256 512 1K 2K 4K 8K 16K 32K 64K 128K 256K 512K 1M; do
+function timestamp() {
+  date +%Y.%m.%d-%H.%M.%S
+}
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE:-$0}" )" &> /dev/null && pwd )"
+SCRIPT_FILE_NAME="$( basename -- "$0" )"
+PROJECT_DIR="$( cd "${SCRIPT_DIR}/../../.." &> /dev/null && pwd )"
+
+OUTPUT_DIR="${PROJECT_DIR}/eval/raw/pegasus/pmem2bench/$(timestamp)"
+mkdir -p "${OUTPUT_DIR}"
+cd "${OUTPUT_DIR}"
+
+JOB_FILE="${SCRIPT_DIR}/${SCRIPT_FILE_NAME%.*}.job.sh"
+
+block_size_list=(
+  64 128 256 512
+  1K 2K 4K 8K 16K 32K 64K 128K 256K 512K
+  1M
+)
+
+# nthreads_list=(
+#   1 2 4 8 16 32 48
+# )
+
+param_source_list=(
+  # devdax
+  fsdax
+  # anon
+)
+
+access_pattern_list=(
+  seq
+  random
+)
+
+queue_name="gpu"
+# devdax_opt="-v USE_DEVDAX=pmemkv -v NUM_DEVDAX=1"
+devdax_opt=""
+
+for param_source in "${param_source_list[@]}"; do
+# for nthreads in "${nthreads_list[@]}"; do
+for block_size in "${block_size_list[@]}"; do
+for ntstore in true false; do
+for access_pattern in "${access_pattern_list[@]}"; do
+
+  # -v PMEM2BENCH_NTHREADS=${nthreads} \
 qsub \
-  -v OUTPUT_DIR \
-  -v NTHREADS=${nthreads} \
-  -v BLOCK_SIZE=${block_size} \
-  ${JOB_SCRIPT}
+  -q ${queue_name} \
+  -A NBB \
+  -b 1 \
+  ${devdax_opt} \
+  -v PROJECT_DIR="${PROJECT_DIR}" \
+  -v OUTPUT_DIR="${OUTPUT_DIR}" \
+  -v PMEM2BENCH_SOURCE=${param_source} \
+  -v PMEM2BENCH_BLOCK_SIZE=${block_size} \
+  -v PMEM2BENCH_NTSTORE=${ntstore} \
+  -v PMEM2BENCH_ACCESS_PATTERN=${access_pattern} \
+  ${JOB_FILE}
 
 done
 done
+done
+done
+# done
